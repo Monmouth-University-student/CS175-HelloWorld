a4165d14589721f3ceef1454e7bdb661d5abcdcf
Jvdollen <55414071+Jvdollen@users.noreply.github.com>
2020-02-06 10:40:28 -0500

    Add files via upload

---
+++ CS175Assignement1.java
@@ -0,0 +1,28 @@
+package CS1751;
+
+public class CS175Assignement1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("39+3");
+			System.out.println(39+3);
+			
+			
+			System.out.println(1+2+3+4+5+6+7+8+9+10);
+		   System.out.println(" + ");
+		  System.out.println("+   +");
+		System.out.println(" +-----+");
+		System.out.println(" | ._. |       ");
+		System.out.println(" | | | |" );
+		System.out.println(" +-+-+-+         ");
+		
+		
+		System.out.println("Miracle on Ice");
+		System.out.println("Lone Survivor");
+		System.out.println("Sandlot");
+		
+			
+
+	}
+
+}
