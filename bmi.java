50e17e30e1dfc07d01a1a54847d4154fbac5e415
Jvdollen <55414071+Jvdollen@users.noreply.github.com>
2020-02-11 18:38:45 -0500

    Add files via upload

---
+++ bmi.java
@@ -0,0 +1,52 @@
+package bmiCalc;
+import java.util.Scanner;
+
+public class bmi {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	 Scanner input = new Scanner(System.in);
+
+
+     double weight;
+     int feet;
+     int inches;
+
+
+     System.out.print("Enter weight in pounds: ");
+     weight = input.nextFloat();
+
+     System.out.print("Enter feet: ");
+     feet = input.nextInt();
+
+     System.out.print("Enter inches: ");
+     inches = input.nextInt();
+
+   
+     double weightInKilos = weight * 0.453592;
+     double heightInMeters = (((feet * 12) + inches) * .0254);
+     double bmi = weightInKilos / Math.pow(heightInMeters, 2.0);
+
+
+   
+     System.out.println("Your BMI is: " + bmi);
+
+ 
+     if (bmi < 18.5 ) {
+         System.out.print("Underweight");
+     }
+
+     else if (bmi >= 18.5 && bmi < 25) {
+         System.out.print("Normal");
+     }
+
+     else if (bmi >= 25 && bmi < 30) {
+         System.out.print("Overweight");
+     }
+
+     else if (bmi >= 30) {
+         System.out.print("Obese");
+     }}}
